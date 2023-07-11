02182f61d6f97edfb192c0493cc160cabc5e5d14
HigorSouza <higor.andrade@hotmail.com>
2023-07-11 15:43:36 -0300

    Telas de autenticação

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custon_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(children: [
+        Expanded(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+            vertical: 40,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(45),
+            ),
+          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            //Email
+            const CustonTextField(
+              icon: Icons.email,
+              label: 'Email',
+            ),
+
+            //senha
+            const CustonTextField(
+              icon: Icons.lock,
+              label: 'Senha',
+              isSecret: true,
+            ),
+
+            //Botão de entrar
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                )),
+                onPressed: () {},
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+
+            //Esqueceu a senha
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Esqueceu a senha?',
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+
+            //Divisor
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Text('Ou'),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Botão de novo user
+            SizedBox(
+              height: 50,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18)),
+                  side: const BorderSide(
+                    width: 2,
+                    color: Colors.green,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Criar conta',
+                  style: c,
+                ),
+              ),
+            ),
+          ]),
+        )
+      ]),
+    );
+  }
+}
