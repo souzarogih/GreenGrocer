c7265ad314a25afe25a718dce2ceec165881f7a7
HigorSouza <higor.andrade@hotmail.com>
2023-07-14 23:43:12 -0300

    segunda parte da tela de home

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -34,17 +34,65 @@ class HomeTab extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: CustomColors.customSwatchColor,
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                backgroundColor: CustomColors.customContrastColor,
+                label: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
             ),
           ),
         ],
       ),
 
       //Campo de pesquisa
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              isDense: true,
+              hintText: 'Pesquise aqui...',
+              hintStyle: TextStyle(
+                color: Colors.grey.shade400,
+                fontSize: 14,
+              ),
+              prefixIcon: Icon(
+                Icons.search,
+                color: CustomColors.customContrastColor,
+                size: 21,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(60),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+            ),
+          ),
+        )
+      ]),
 
       //Categorias
 
