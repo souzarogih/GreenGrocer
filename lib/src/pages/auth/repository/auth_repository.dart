0c4fc503937e3880ff8d8d7483e74cc5dc2554d7
HigorSouza <higor.andrade@hotmail.com>
2023-08-01 14:07:02 -0300

    Serializando o model do Auth

--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class AuthRepository {
@@ -14,8 +15,10 @@ class AuthRepository {
     );
 
     if (result['result'] != null) {
-      print('Signin funcionou!');
-      print(result['result']);
+      // print('Signin funcionou!');
+      // print(result['result']);
+      final user = UserModel.fromMap(result['result']);
+      print(user);
     } else {
       print('Signin não funcionou!');
       print(result['error']);
