3b23139774dae986f7d7e848e24b26f04c593039
HigorSouza <higor.andrade@hotmail.com>
2023-08-08 13:42:11 -0300

    Buscando categorias do backend.

--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,5 +1,6 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 import 'dart:developer' as dev;
@@ -27,6 +28,27 @@ class HomeRepository {
       // print(result['result']);
       return HomeResult.error(
           'Ocorreu um erro inesperado ao recuperar as categorias.');
+    }
+  }
+
+  Future<HomeResult<ItemModel>> getAllProducts(
+      Map<String, dynamic> body) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.getAllProducts,
+      method: HttpMethods.post,
+      body: body,
+    );
+
+    if (result['result'] != null) {
+      List<ItemModel> data = List<Map<String, dynamic>>.from(result['result'])
+          .map(ItemModel.fromJson)
+          .toList();
+      dev.log('Itens recuperados com sucesso.');
+      return HomeResult<ItemModel>.success(data);
+    } else {
+      dev.log('GG-105 Ocorreu um erro inesperado ao recuperar os itens.');
+      return HomeResult.error(
+          'Ocorreu um erro inesperado ao recuperar os itens.');
     }
   }
 }
